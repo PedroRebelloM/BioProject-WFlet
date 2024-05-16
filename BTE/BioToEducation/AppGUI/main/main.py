b005fb3de37f813910cb36ff916facf5734bcee7
Ratex <prmorais2006@gmail.com>
2024-05-16 19:36:52 -0300

    Importado os módulos para criar a barra de navegação

---
+++ BTE/BioToEducation/AppGUI/main/main.py
@@ -0,0 +1,6 @@
+import flet as ft
+from flet import View, Page, AppBar, ElevatedButton, Text
+from flet import RouteChangeEvent, ViewPopEvent, CrossAxisAlignment, MainAxisAlignment
+
+def main(page: ft.Page) -> None:
+    page.title = 'BTE - Biology To Education'
