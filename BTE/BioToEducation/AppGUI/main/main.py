bee7879bf7db53669af338a35af96b916e9f1083
Ratex <prmorais2006@gmail.com>
2024-05-16 20:24:42 -0300

    Criação das divisões iniciais

--- BTE/BioToEducation/AppGUI/main/main.py
+++ BTE/BioToEducation/AppGUI/main/main.py
@@ -4,3 +4,26 @@ from flet import RouteChangeEvent, ViewPopEvent, CrossAxisAlignment, MainAxisAli
 
 def main(page: ft.Page) -> None:
     page.title = 'BTE - Biology To Education'
+    
+    page.add(
+        ft.Row(
+            [
+                 ft.VerticalDivider(),
+                ft.Container(
+                    bgcolor= "#A1FF0A",
+                    alignment=ft.alignment.center,
+                    expand=False,
+                    width=140
+                ),
+                ft.Container(
+                    bgcolor= "#FFFFFF",
+                    alignment=ft.alignment.center,
+                    expand=True,
+                ),
+                
+            ],
+            spacing=0,
+            expand=True,
+        )
+    )
+ft.app(target=main)
