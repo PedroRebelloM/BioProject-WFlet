c984dc6949974ee9e5d22379cc37c9b1a07949db
Ratex <prmorais2006@gmail.com>
2024-05-17 23:01:43 -0300

    Posicionando os botões temporários

--- BTE/BioToEducation/AppGUI/main/main.py
+++ BTE/BioToEducation/AppGUI/main/main.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
         
         #Logo BTE
         img = ft.Image(
-                src = r"C:\Users\Pedro\Desktop\BioProject-WFlet\BTE\BioToEducation\AppGUI\source\Logo.png",
+                src = r"C:\Users\Pedro\Desktop\BioProject-WFlet\BTE\BioToEducation\AppGUI\assets\icons\Logo.png",
                 width= 200,
                 height= 200,
                 fit = ft.ImageFit.CONTAIN,
@@ -18,21 +18,47 @@ if __name__ == "__main__":
                 )
         
         iconHome = ft.IconButton(
-                icon = ft.icons.HOME, on_click=None, data=0
+                icon = ft.icons.HOME, on_click=None, data=0,
+                icon_color = "black",
+                icon_size = 40,
+                tooltip = "Home"
                 
         )
         
+        
+        colunaSuperior = ft.Column(
+            [
+                img
+            ],
+            alignment = ft.MainAxisAlignment.START,
+            horizontal_alignment= ft.CrossAxisAlignment.START
+            
+        )
+        
+        colunaDoMeio = ft.Column(
+            [
+                iconHome
+            ],
+            alignment= ft.MainAxisAlignment.CENTER,
+            horizontal_alignment = ft.CrossAxisAlignment.CENTER
+            
+        )
         layout = ft.Row(
                 [
                     ft.VerticalDivider(),
                     ft.Container(
                         bgcolor= "#A1FF0A",
-                        alignment=ft.alignment.top_center,
                         expand=False,
                         width=140,
                         border = ft.border.all(0.5, ft.colors.BLACK),
                         border_radius = ft.border_radius.BorderRadius(20, 0, 20, 0),
-                        content = img
+                        content = ft.Column(
+                            [
+                               colunaSuperior, colunaDoMeio
+                            ],
+                            horizontal_alignment = ft.CrossAxisAlignment.CENTER
+                          
+                        )
                     )                 
                     ,
                     ft.Container(
@@ -51,4 +77,4 @@ if __name__ == "__main__":
         page.add(layout, iconHome)
         
 
-ft.app(target=main, assets_dir="../source")
+ft.app(target=main, assets_dir="../assets/icons")
