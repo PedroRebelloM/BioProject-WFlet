7fb8dc2e7152782624fd94daa3567410ee504efc
Ratex <prmorais2006@gmail.com>
2024-05-17 21:01:10 -0300

    Layout parcialmente feito

--- BTE/BioToEducation/AppGUI/main/main.py
+++ BTE/BioToEducation/AppGUI/main/main.py
@@ -2,28 +2,53 @@ import flet as ft
 from flet import View, Page, AppBar, ElevatedButton, Text
 from flet import RouteChangeEvent, ViewPopEvent, CrossAxisAlignment, MainAxisAlignment
 
-def main(page: ft.Page) -> None:
-    page.title = 'BTE - Biology To Education'
+if __name__ == "__main__":
     
-    page.add(
-        ft.Row(
-            [
-                 ft.VerticalDivider(),
-                ft.Container(
-                    bgcolor= "#A1FF0A",
-                    alignment=ft.alignment.center,
-                    expand=False,
-                    width=140
-                ),
-                ft.Container(
-                    bgcolor= "#FFFFFF",
-                    alignment=ft.alignment.center,
-                    expand=True,
-                ),
+    def main(page: ft.Page) -> None:
+        page.title = 'BTE - Biology To Education'
+        
+        #Logo BTE
+        img = ft.Image(
+                src = r"C:\Users\Pedro\Desktop\BioProject-WFlet\BTE\BioToEducation\AppGUI\source\Logo.png",
+                width= 200,
+                height= 200,
+                fit = ft.ImageFit.CONTAIN,
+                filter_quality="HIGH",
                 
-            ],
-            spacing=0,
-            expand=True,
+                )
+        
+        iconHome = ft.IconButton(
+                icon = ft.icons.HOME, on_click=None, data=0
+                
         )
-    )
-ft.app(target=main)
+        
+        layout = ft.Row(
+                [
+                    ft.VerticalDivider(),
+                    ft.Container(
+                        bgcolor= "#A1FF0A",
+                        alignment=ft.alignment.top_center,
+                        expand=False,
+                        width=140,
+                        border = ft.border.all(0.5, ft.colors.BLACK),
+                        border_radius = ft.border_radius.BorderRadius(20, 0, 20, 0),
+                        content = img
+                    )                 
+                    ,
+                    ft.Container(
+                        bgcolor= "#FFFFFF",
+                        alignment=ft.alignment.center,
+                        expand=True,
+                        border = ft.border.all(0.5, ft.colors.BLACK),
+                        border_radius= ft.border_radius.BorderRadius(0, 20, 0, 20)
+                    ),
+                    
+                ],
+                spacing=0,
+                expand=True,
+            )
+
+        page.add(layout, iconHome)
+        
+
+ft.app(target=main, assets_dir="../source")
