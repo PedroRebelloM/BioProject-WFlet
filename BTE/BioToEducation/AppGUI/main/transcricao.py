3cdeccc1dfd398acfa1877f0067d5b99fbec5b92
Ratex <prmorais2006@gmail.com>
2024-05-30 13:48:03 -0300

    Remoção das funções dos botões de transcrição

--- BTE/BioToEducation/AppGUI/main/transcricao.py
+++ BTE/BioToEducation/AppGUI/main/transcricao.py
@@ -129,7 +129,7 @@ def CriarLayoutTranscricao(page: ft.Page):
                 ft.MaterialState.DEFAULT: ft.BorderSide(1, ft.colors.BLACK), 
                     
             }
-        ), adaptive = True, width = 400, height = 30, on_click=lambda _: funcoes.botaoA(linha) 
+        ), adaptive = True, width = 400, height = 30, on_click = None
     )
     
     #Botao do Gene B
@@ -140,7 +140,7 @@ def CriarLayoutTranscricao(page: ft.Page):
                 ft.MaterialState.DEFAULT: ft.BorderSide(1, ft.colors.BLACK), 
                     
             }
-        ), adaptive = True, width = 400, height = 30, on_click=lambda _: funcoes.botaoB(linha)
+        ), adaptive = True, width = 400, height = 30, on_click = None
     )
         
     
