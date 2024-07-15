2d80e9a1b648c3b23969d61a505f3f5cf2bef352
Ratex <prmorais2006@gmail.com>
2024-07-15 20:33:42 -0300

    Alterando temporariamente o local da pasta database

--- BTE/BioToEducation/AppGUI/main/funcoes.py
+++ BTE/BioToEducation/AppGUI/main/funcoes.py
@@ -2,10 +2,8 @@ import flet as ft
 import os 
 import sys 
 import layout, traducao, transcricao, comparacao, login
-import clipboard as cp
 
 root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', '..'))
-
 
 if root_dir not in sys.path:
     sys.path.append(root_dir)
